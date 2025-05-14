3dc15c0743efd013d2c0cf151edebd0be9dd419b
weichan <111043280+aweich@users.noreply.github.com>
2025-05-14 13:48:16 +0200

    tried removing files from being staged

--- stack_simulations.sh
+++ stack_simulations.sh
@@ -1,5 +1,53 @@
 #!/bin/bash
 
+for config in case2_VCN/5_configs/*; do
+	echo "Processing: $config"
+	esloco "$config"
+	plot_esloco "$config"
+done
+
+for config in case2_VCN/7_configs/*; do
+	echo "Processing: $config"
+	esloco "$config"
+	plot_esloco "$config"
+done
+
+for config in case2_VCN/10_configs/*; do
+	echo "Processing: $config"
+	esloco "$config"
+	plot_esloco "$config"
+done
+
+for config in case2_VCN/12_configs/*; do
+	echo "Processing: $config"
+	esloco "$config"
+	plot_esloco "$config"
+done
+
+for config in case2_VCN/15_configs/*; do
+	echo "Processing: $config"
+	esloco "$config"
+	plot_esloco "$config"
+done
+
+for config in case2_VCN/dominance_experiment/10dd_configs/*; do
+	echo "Processing: $config"
+	esloco "$config"
+	plot_esloco "$config"
+done
+
+for config in case2_VCN/dominance_experiment/1dd_configs/*; do
+	echo "Processing: $config"
+	esloco "$config"
+	plot_esloco "$config"
+done
+
+for config in case2_VCN/dominance_experiment/01dd_configs/*; do
+	echo "Processing: $config"
+	esloco "$config"
+	plot_esloco "$config"
+done
+
 for config in case2_VCN/leftover/*; do
 	echo "Processing: $config"
 	esloco "$config"
